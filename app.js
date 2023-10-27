ac5ce522785e4ae3bc99805a8623dd40c52ac3fd
FATİH BAYIR <fatihhusnub@gmail.com>
2023-10-27 14:44:16 +0300

    Çalışma Güncellendi

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
-const fn = function (){
-    console.log("Function is Working");
-}
+const express = require('express');
+
+const app = express();
+
+app.get('/', (reg, res) => {
+  const photo = {
+    id: 1,
+    title: 'Blog title',
+    description: 'Blog description',
+  };
+
+  res.send(photo);
+});
+
+const port = 3001;
+
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda başlatıldı..     `);
+});
