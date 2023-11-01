1ec3ef4249fd67d1cfb7bca2ca87131149d73921
FATİH BAYIR <fatihhusnub@gmail.com>
2023-11-01 11:01:04 +0300

    Çalışma güncellendi..

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
 const express = require('express');
-
+const ejs = require("ejs");
+const  path = require("path");
 const app = express();
 
-app.get('/', (reg, res) => {
-  const photo = {
-    id: 1,
-    title: 'Blog title',
-    description: 'Blog description',
-  };
 
-  res.send(photo);
+//TEMPLATE ENGİNE
+app.set("view engine","ejs");
+
+
+
+//MIDDLEWARE
+app.use(express.static('public'));
+
+
+// ROUTES
+app.get('/', (reg, res) => {
+    res.render("index")
+});
+
+app.get('/about', (reg, res) => {
+  res.render("about")
+});
+
+app.get('/add', (reg, res) => {
+  res.render("add")
+});
+
+app.get('/post', (reg, res) => {
+  res.render("post")
 });
 
 const port = 3001;
